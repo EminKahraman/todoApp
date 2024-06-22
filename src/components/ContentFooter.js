81ea41ec4900af3dd9c23b715ea7a92dc8d97a45
EminKahraman <eminmkahraman@gmail.com>
2024-06-22 21:28:13 +0300

    first commit

---
+++ src/components/ContentFooter.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { useSelector } from 'react-redux'
+
+function ContentFooter() {
+	const items = useSelector((state) => state.todos.items);
+	const itemsLeft = items.filter(item => !item.completed).length
+
+	
+  return ( 
+    <footer className="footer">
+		<span className="todo-count">
+			<strong>{itemsLeft}</strong> item{itemsLeft > 1 && 's'} left
+		</span>
+
+		<ul className="filters">
+			<li>
+				<a href="#/" className="selected">All</a>
+			</li>
+			<li>
+				<a href="#/">Active</a>
+			</li>
+			<li>
+				<a href="#/">Completed</a>
+			</li>
+		</ul>
+
+		<button className="clear-completed">
+			Clear completed
+		</button>
+	</footer>
+  )
+}
+
+export default ContentFooter
